831b75e52ccf0b9c0dcbb5929e46a9546e2f5009
Gabriel Malavolta <gabriel.malavolta@gmail.com>
2021-11-03 00:57:59 -0300

    Commit

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_lenght=255)
+    title = models.CharField(max_length = 255)
     slug = models.SlugField(max_length=255, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    
+
