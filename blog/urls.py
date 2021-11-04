a27716394750aa5cb8312c4b68c03f4fadd38a77
Gabriel Malavolta <gabriel.malavolta@gmail.com>
2021-11-03 21:57:03 -0300

    Commit

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.views.generic.base import TemplateResponseMixin
+from . import views
+ 
+
+app_name = "blog"
+
+urlpatterns = [
+    path("", views.PostListView.as_view(), name="list"),
+    path("<slug:slug>/", views.PostDetailView.as_view(), name="detail"),
+    Teste 
+    
+]
+
